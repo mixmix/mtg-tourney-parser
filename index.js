4e6c3abc9bcd03b94c00b79909d14bb28da646dc
mix irving <whimful@gmail.com>
2016-07-02 18:48:37 +1200

    bump version, add README

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-import extract from 'pdf-text-extract'
+var extract = require('pdf-text-extract')
+
+var extractTableRows  = require('./utils/extractTableRows')
+var extractColumnData = require('./utils/extractColumnData')
+var extractedRowData  = require('./utils/extractRowData')
+
+function parser (filePath, callback) {
+  extract(filePath, (err, text) => {
+    if (err) {
+      callback(err)
+      return
+    }
+
+    var lines = text.join('\n').split('\n')
+    var tableRows = extractTableRows(lines)
+    var columnData = extractColumnData(tableRows.head)
+    var rowData = extractedRowData(tableRows.body, columnData)
+
+    callback(null, rowData)
+
+  })
+}
+
+module.exports = parser
+
 
